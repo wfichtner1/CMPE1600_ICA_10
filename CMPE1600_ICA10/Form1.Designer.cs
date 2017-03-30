efec20477120fc28909c1708289d5cd0db0973a1
William Fichtner <william_25_35@hotmail.com>
2017-03-29 21:20:15 -0600

    done

--- CMPE1600_ICA10/Form1.Designer.cs
+++ CMPE1600_ICA10/Form1.Designer.cs
@@ -33,49 +33,67 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
             // 
             // trackBar1
             // 
-            this.trackBar1.Location = new System.Drawing.Point(12, 41);
+            this.trackBar1.Location = new System.Drawing.Point(9, 33);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.trackBar1.Maximum = 5000;
             this.trackBar1.Minimum = 100;
             this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Size = new System.Drawing.Size(258, 56);
+            this.trackBar1.Size = new System.Drawing.Size(194, 45);
             this.trackBar1.TabIndex = 0;
+            this.trackBar1.TickFrequency = 250;
             this.trackBar1.Value = 100;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 100);
+            this.label1.Location = new System.Drawing.Point(9, 81);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(32, 17);
+            this.label1.Size = new System.Drawing.Size(25, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "100";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(224, 100);
+            this.label2.Location = new System.Drawing.Point(168, 81);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(40, 17);
+            this.label2.Size = new System.Drawing.Size(31, 13);
             this.label2.TabIndex = 2;
             this.label2.Text = "5000";
             // 
             // timer1
             // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(56, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Number of Iterations";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.ClientSize = new System.Drawing.Size(212, 138);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.trackBar1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
@@ -90,6 +108,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- CMPE1600_ICA10/Form1.cs
+++ CMPE1600_ICA10/Form1.cs
@@ -5,15 +5,31 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 using GDIDrawer;
 
 namespace CMPE1600_ICA10
 {
     public partial class Form1 : Form
     {
-        CDrawer canvas = new CDrawer(800,600, false);
+        public struct MousePoint
+        {
+            public Point _point;
+            public int _drawAmount;
+
+            public MousePoint(Point point, int pixels)
+            {
+                _point = point;
+                _drawAmount = pixels;
+            }
+        }
+
+        static CDrawer canvas = new CDrawer(800,600, false);
+        static object placeHolder = new object();
+        static object passer = new object();
 
         public Form1()
         {
@@ -22,7 +38,51 @@ namespace CMPE1600_ICA10
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+            MousePoint userClick = new MousePoint();
+            Point point;
+            if(canvas.GetLastMouseLeftClickScaled(out point))
+            {
+                userClick = new MousePoint(point, trackBar1.Value);
+                passer = (object)userClick;
+                Thread threadOne = new Thread(Wanderer);
+                threadOne.Start(passer);                
+            }
+        }
 
+        static void Wanderer(object holder)
+        {
+            MousePoint start = (MousePoint)holder;
+            Random rand = new Random();
+            Color drawCol = RandColor.GetKnownColor();
+            Point velocity = new Point(1, 1);
+            Point temp = start._point;
+            lock (placeHolder)
+            {
+                canvas.SetBBScaledPixel(temp.X += velocity.X, temp.Y += velocity.Y, drawCol);
+                canvas.Render();
+            }
+
+            for (int i = 0; i < start._drawAmount; i++)
+            {
+                //drawCol = RandColor.GetKnownColor();
+                temp.X += rand.Next(-1, 2);
+                temp.Y += rand.Next(-1, 2);
+                temp.X = (temp.X < 0) ? 0 : temp.X;
+                temp.Y = (temp.Y < 0) ? 0 : temp.Y;
+                temp.X = (temp.X > 799) ? 0 : temp.X;
+                temp.Y = (temp.Y > 599) ? 0 : temp.Y;
+
+                lock(placeHolder)
+                {                    
+                    canvas.SetBBScaledPixel(temp.X, temp.Y, drawCol);
+                    canvas.Render();
+                    Thread.Sleep(1);
+                }
+            }
+
+            
+
+            
         }
     }
 }
